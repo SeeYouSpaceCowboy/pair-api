46f8bb952063f2d4a2c8c91bcebb1aab12497e4b
hassey13 <ehasselbring@gmail.com>
2017-02-13 15:41:09 -0500

    added gems, routes, controller inits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :stocks
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :stocks
+      resources :users
+      post 'signup', to 'users#create'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
