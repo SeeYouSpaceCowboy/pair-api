3c645b7660a5bddb104cd3edc018783d29cee25d
hassey13 <ehasselbring@gmail.com>
2017-02-15 15:32:31 -0500

    added stockdata controller

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'dotenv-rails'
 gem 'rest-client'
 gem 'active_model_serializers'
 gem 'jwt'
+gem 'json'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       resources :users
       post 'signup', to: 'users#create', as: 'signup'
       post 'login', to: 'users#login', as: 'login'
+      get 'fetchstocks', to: 'stock_data#index', as: 'fetchstocks'
+      get 'searchstocks/:id', to: 'stock_data#check_for_stock', as: 'searchstocks'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
