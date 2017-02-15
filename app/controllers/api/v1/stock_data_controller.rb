b143406a3664bc8afa964459b8ddb232c471f88d
hassey13 <ehasselbring@gmail.com>
2017-02-15 17:16:22 -0500

    refactored stock-data controller

--- app/controllers/api/v1/stock_data_controller.rb
+++ app/controllers/api/v1/stock_data_controller.rb
@@ -6,28 +6,48 @@ require "base64"
 class Api::V1::StockDataController < ApplicationController
   before_action :set_stock, only: [:show, :update, :destroy]
 
-  def index
-    url = 'https://api.intrinio.com/data_point?ticker=A&item=last_price'
 
+  # Market Cap
+  # Day range
+  # 52-week range
+  # Dividend & Yield
+
+  def index
     @stocks = Stock.all
-    all_stocks = RestClient.get(url, {:'Authorization' => password })
-    response = JSON.parse(all_stocks)
-    print response
-    render json: response
+    if @stocks.length > 1
+      stock_list = @stocks.collect {|stock| stock.symbol}
+
+      # historical_data endpoint
+      # https://api.intrinio.com/historical_data?identifier={symbol}&item={tag}
+      url = "https://api.intrinio.com/data_point?ticker=#{stock_list.join(',')}&F&item=last_price"
+
+      response = api_call(url)
+      render json: response
+    else
+      render json: {data: 'no stocks'}
+    end
   end
 
   def check_for_stock
-
     url = "https://api.intrinio.com/companies?query=#{params[:id]}"
-    all_stocks = RestClient.get(url, {:'Authorization' => password })
-    response = JSON.parse(all_stocks)
+
+    response = api_call(url)
     render json: response
 
   end
 
   def show
+    url = "https://api.intrinio.com/data_point?ticker=#{params[:id]}&item=last_price"
 
+    response = api_call(url)
+    render json: response
   end
+
+
+  #news endpoint
+  # https://api.intrinio.com/news?ticker=AAPL&ticker=MSFT
+
+
 
   private
 
