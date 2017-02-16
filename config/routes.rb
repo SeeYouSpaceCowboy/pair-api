6cdb99255c44032e9172014202c59da1727fbe9b
hassey13 <ehasselbring@gmail.com>
2017-02-16 12:14:09 -0500

    delete stock functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :stocks
       resources :users
+      post 'unfollow', to: 'stocks#destroy'
       post 'signup', to: 'users#create', as: 'signup'
       post 'login', to: 'users#login', as: 'login'
       get 'fetchstocks', to: 'stock_data#index', as: 'fetchstocks'
