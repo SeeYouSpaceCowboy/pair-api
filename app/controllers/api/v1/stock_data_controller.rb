0356e5848fd9347a33b568b88420f96bc661f652
hassey13 <ehasselbring@gmail.com>
2017-02-16 18:35:42 -0500

    disconnect api for dev env

--- app/controllers/api/v1/stock_data_controller.rb
+++ app/controllers/api/v1/stock_data_controller.rb
@@ -17,20 +17,32 @@ class Api::V1::StockDataController < ApplicationController
     @stocks = get_current_user.stocks
     if @stocks.length > 0
       stock_list = @stocks.collect {|stock|
-
-      date = "2017-02-15"
-      url = "https://api.intrinio.com/prices?ticker=#{stock.symbol}&start_date=#{date}&end_date=#{date}"
-
-      response = api_call(url)
-      response[:company_name] = stock.company_name
-      response[:identifier] = stock.symbol
-
-      url = "https://api.intrinio.com/historical_data?ticker=#{stock.symbol}&item=marketcap&start_date=#{date}&end_date=#{date}"
-      market_cap = api_call(url)
-
-      response["data"][0][:market_cap] = market_cap["data"][0]["value"]
-
-      response
+    #
+    #   date = "2017-02-15"
+    #   url = "https://api.intrinio.com/prices?ticker=#{stock.symbol}&start_date=#{date}&end_date=#{date}"
+    #
+    #   response = api_call(url)
+    #   response[:company_name] = stock.company_name
+    #   response[:identifier] = stock.symbol
+    #
+    #   url = "https://api.intrinio.com/historical_data?ticker=#{stock.symbol}&item=marketcap&start_date=#{date}&end_date=#{date}"
+    #   market_cap = api_call(url)
+    #
+    #   response["data"][0][:market_cap] = market_cap["data"][0]["value"]
+    #
+    #   response
+        stock_data = {
+          identifier: "#{stock.symbol}",
+          company_name: "#{stock.company_name}",
+          data: [{
+            open: 1,
+            close: 1,
+            high: 1,
+            low: 1,
+            market_cap: 1
+          }]
+        }
+        stock_data
       }
       render json: stock_list
     else
