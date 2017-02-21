b4239c8a1ffc880f7ee4987b4651b0af685e562c
hassey13 <ehasselbring@gmail.com>
2017-02-21 14:00:00 -0500

    added friends api support

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :comments
       resources :stocks
       resources :users
+      resources :following
       post 'unfollow', to: 'stocks#destroy'
       post 'signup', to: 'users#create', as: 'signup'
       post 'login', to: 'users#login', as: 'login'
