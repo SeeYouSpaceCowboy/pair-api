66dc4e0efe2c2c39a26df00875c33bad5ea963b2
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-03-16 18:15:34 -0400

    Changes to followers model

--- db/migrate/20170213170533_create_user_following.rb
+++ db/migrate/20170213170533_create_user_following.rb
@@ -1,12 +1,12 @@
 class CreateUserFollowing < ActiveRecord::Migration[5.0]
   def change
-    create_table :user_followings ,id: false do |t|
+    create_table :user_following ,id: false do |t|
       t.integer :user_id
       t.integer :followee_id
       t.timestamps
     end
 
-    add_index(:user_followings, [:user_id, :followee_id], :unique => true)
-    add_index(:user_followings, [:followee_id, :user_id], :unique => true)
+    add_index(:user_following, [:user_id, :followee_id], :unique => true)
+    add_index(:user_following, [:followee_id, :user_id], :unique => true)
   end
 end
